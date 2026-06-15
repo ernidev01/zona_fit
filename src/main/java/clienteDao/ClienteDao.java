a253adce2dbda1cafa86d02d4ead04565817915d
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2026-06-15 11:14:03 -0500

    Creacion de buscar cliente por id

--- src/main/java/clienteDao/ClienteDao.java
+++ src/main/java/clienteDao/ClienteDao.java
@@ -9,41 +9,59 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ClienteDao implements IClienteDao{
+public class ClienteDao implements IClienteDao {
+    PreparedStatement ps;
+    ResultSet rs;
+    Connection conexion = Conexion.getConection();
+
     @Override
     public List<Cliente> listarClientes() {
-        List <Cliente> clientes = new ArrayList<>();
-        PreparedStatement ps;
-        ResultSet rs;
-        Connection conexion = Conexion.getConection();
+        List<Cliente> clientes = new ArrayList<>();
         var select = "SELECT * FROM cliente ORDER BY idcliente";
-        try{
-            ps= conexion.prepareStatement(select);
+        try {
+            ps = conexion.prepareStatement(select);
             rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 var cliente = new Cliente();
                 cliente.setId(rs.getInt("id"));
                 cliente.setNombre(rs.getString("nombre"));
                 cliente.setApellido(rs.getString("apellido"));
                 cliente.setMembresia(rs.getInt("membresia"));
-            clientes.add(cliente);
+                clientes.add(cliente);
             }
 
         } catch (Exception e) {
-            System.out.println("Error al listas los clientes: " +e.getMessage());
-        }
-        finally {
-            try {
-                conexion.close();
-            }catch (Exception e){
-                System.out.println("Error al cerrar la conexion: " +e.getMessage() );
-            }
+            System.out.println("Error al listas los clientes: " + e.getMessage());
+        } finally {
+            cerrarConexion();
         }
         return clientes;
     }
 
     @Override
     public boolean buscarClinete(Cliente cliente) {
+        var select = "SELECT * FROM cliente WHERE idcliente =?";
+        try {
+            ps = conexion.prepareStatement(select);
+            ps.setInt(1, cliente.getId());
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                return true;
+            }
+            else {
+                System.out.println("No existe el cliente con el id: "+cliente.getId());
+            }
+        } catch (Exception e) {
+            System.out.println("Error al buscaar el cliente por el id: " + e.getMessage());
+        }
+        finally {
+            cerrarConexion();
+        }
+
         return false;
     }
 
@@ -60,5 +78,13 @@ public class ClienteDao implements IClienteDao{
     @Override
     public boolean eliminarCliente(Cliente cliente) {
         return false;
+    }
+
+    public void cerrarConexion() {
+        try {
+            conexion.close();
+        } catch (Exception e) {
+            System.out.println("Error al cerrar la conexion: " + e.getMessage());
+        }
     }
 }
